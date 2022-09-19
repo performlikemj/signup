b81870a91736e1cac588be0bccd5d7729dbe2cd7
MJ <mikey.j.1228@gmail.com>
2022-09-19 16:06:22 +0900

    Refactor compare function to add text when no match and remove text if there is a match

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Script to add text and red border when password fields do not match
-
+let match = true;
+let para = document.createElement('p');
+para.innerText = '* Passwords do not match';
+para.style.color = 'red';
 // Compare the password fields function and add red border and text
 function compare(str1, str2) {
-    if (str1 != str2) {
+    if (para.parentNode === null && str1 != str2) {
         const passwordWrapper = document.querySelector('form');
-        let para = document.createElement('p');
-        para.innerText = '* Passwords do not match';
-        para.style.color = 'red';
         passwordWrapper.appendChild(para);
-
     }
+    if (para.parentNode != null && str1 === str2) {
+        para.parentNode.removeChild(para);
+    }
+
+    // if (str1 != str2) {
+
+    //     match = false;
+    //     const passwordWrapper = document.querySelector('form');
+    //     passwordWrapper.appendChild(para);
+    // } 
+    // if (str1 === str2) {
+    //     para.remove();
+
+    // }
 }
+
+
